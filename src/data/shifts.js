9a181433bc62b70af59b87f3ab2796bc8b06dbdd
Andrii <andreychaika87@gmail.com>
2024-09-08 19:25:57 +0200

    Update shifts.js

--- src/data/shifts.js
+++ src/data/shifts.js
@@ -1,7 +1,7 @@
 const shifts = [
-  "1 Shift (6:00-14:00)",
-  "2 Shift (14:00-22:00)",
-  "3 Shift (22:00-6:00)",
+  "1st Shift (6:00-14:00)",
+  "2nd Shift (14:00-22:00)",
+  "3rd Shift (22:00-6:00)",
 ];
 
 export default shifts;
